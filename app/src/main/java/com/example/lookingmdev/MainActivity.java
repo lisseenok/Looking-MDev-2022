4dda4108fc86b19fc3961502fd9818b558c08544
Alastorial <alex.babykin2003@yandex.ru>
2022-04-20 20:39:00 +0300

    style fragment_search.xml

--- app/src/main/java/com/example/lookingmdev/MainActivity.java
+++ app/src/main/java/com/example/lookingmdev/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.lookingmdev;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -23,11 +25,11 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        BottomNavigationView navView = findViewById(R.id.nav_view); // объект панели навигации
-
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_search, R.id.navigation_saved, R.id.navigation_booking)
-                .build();
+//        BottomNavigationView navView = findViewById(R.id.nav_view); // объект панели навигации
+//
+//        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+//                R.id.navigation_search, R.id.navigation_saved, R.id.navigation_booking)
+//                .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         //NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
